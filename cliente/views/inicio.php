8e3318affc3a006baa8944f4685c4e4e99508861
Joseignacione <91512909+Joseignacione@users.noreply.github.com>
2022-05-01 19:50:40 -0400

    Add files via upload

---
+++ cliente/views/inicio.php
@@ -0,0 +1,26 @@
+<div class="container my-3 slider-updown">
+    <div class="row d-flex justify-content-center">
+        <div class="col-11 col-sm-11 col-lg-8 bg-white rounded border shadow-sm">
+            <h3 class="text-center font-weight-bold my-4">Web Service REST</h3>
+            <hr>
+            <p class="text-justify">Implementación web realizada para el ramo de Redes y Comunicación de Datos, con la
+                finalidad de emplear un servicio web a travez del uso de una <strong>API Rest</strong> creada con NodeJS
+                como <a class="badge badge-primary">backend</a> y PHP + Bootstrap como <a class="badge badge-danger">frontend</a>.</p>
+            <p>Podrás ocupar 2 servicios, comprobar si el digito verificador ingresado es valido y obtener un saludo a
+                partir del nombre y sexo ingresados.</p>
+            <p>Integrantes:</p>
+            <div class="d-flex justify-content-center">
+                <ul class="list-group list-group-horizontal-md">
+                    <li class="list-group-item py-1">Virla Parra</li>
+                    <li class="list-group-item py-1">Jose Hervas</li>
+                    <li class="list-group-item py-1">Nicolas Rojas</li>
+                    <li class="list-group-item py-1">Lucas Poffal</li>
+                    <li class="list-group-item py-1">Roberto Castillo</li>
+                    <li class="list-group-item py-1">Matias Rubilar</li>
+                </ul>
+            </div>
+            
+        </div>
+    </div>
+</div>
+<br>
