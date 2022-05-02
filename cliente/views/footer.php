8335da526af1e53aa2485b67376b754d0d96feab
rvbilvr <72248190+rvbilvr@users.noreply.github.com>
2022-05-01 20:03:41 -0400

    Carpeta cliente borrada.

--- cliente/views/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="container-fluid bg-teal fixed-bottom">
-    <div class="d-flex justify-content-center">
-        <p class="text-white my-0">Copyright <i class="far fa-copyright"></i> 2022 | UTEM</p>
-    </div>
-</div>
